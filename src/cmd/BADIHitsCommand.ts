001e4ec12ffc6c1062fad1062d80fd322b9cdf7a
Mario Kernich <mario.kernich@swan.de>
2022-11-18 20:04:09 +0100

    improve badi hits command

--- src/cmd/BADIHitsCommand.ts
+++ src/cmd/BADIHitsCommand.ts
@@ -3,6 +3,7 @@ import { Command } from "../Command.js";
 
 export class BADIHitsCommand implements Command {
   name = "badi-hits";
+  aliases = ["badihits", "badi", "badis", "badies", "body"];
 
   async execute() {
     await Terminal.writelns(
@@ -10,10 +11,14 @@ export class BADIHitsCommand implements Command {
         "<br>",
         "<b>Die Aktuellen TOP SAP BAdI Hits</b>",
         "<br>",
-        "- Ain't no BAdI",
-        "- My BAdI is over the ocean",
-        "- No BAdI - no cry",
-        "<br>"
+        "💿 Ain't no BAdI",
+        "💿 My BAdI is over the ocean",
+        "💿 No BAdI - no cry",
+        "💿 Rock your BAdI",
+        "💿 Some BAdI I used to know",
+        "💿 Don't stop the BAdI",
+        "💿 Everybadi (Backstreet's back)",
+        "<br>",
       ],
       "color2 margin"
     );
