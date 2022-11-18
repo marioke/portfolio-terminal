dc3bd4f3b8f06efe4db590ef9875c01a9b7e5b52
Mario Kernich <mario.kernich@swan.de>
2022-11-18 20:05:25 +0100

    print a fake "ping" process

--- src/cmd/PingCommand.ts
+++ src/cmd/PingCommand.ts
@@ -5,6 +5,36 @@ export class PingCommand implements Command {
   name = "ping";
 
   async execute() {
-    await Terminal.writeln("PONG! 🏓", "color2");
+    const ip = this.getRandomIp();
+    const counter = 5;
+    for (let i = 0; i < counter; i++) {
+      await Terminal.writeln(
+        `64 bytes from ${ip}: icmp_seq=${i} ttl=112 time=${Math.floor(
+          Math.random() * 100
+        )}.${Math.floor(Math.random() * 500)} ms`,
+        "color2"
+      );
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+    await Terminal.writeln(`--- ping statistics ---`, "color2");
+    await Terminal.writeln(
+      `${counter} packets transmitted, ${counter} PONGS 🏓 received, 0% packet loss, time ${
+        counter * 500
+      }ms`,
+      "color2"
+    );
+  }
+
+  private getRandomIp(): string {
+    return (
+      Math.floor(Math.random() * 255) +
+      1 +
+      "." +
+      Math.floor(Math.random() * 255) +
+      "." +
+      Math.floor(Math.random() * 255) +
+      "." +
+      Math.floor(Math.random() * 255)
+    );
   }
 }
